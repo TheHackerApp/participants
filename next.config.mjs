9b3e5128ea3d7c443e9eec08ca7d24f5e11401da
Alex Krantz <alex@krantz.dev>
2024-05-23 10:27:58 -0700

    Fix generated graphql resolution

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    const extensionAlias = config.resolve.extensionAlias ?? {};
+    extensionAlias['.graphql'] = ['.graphql.ts'];
+    config.resolve.extensionAlias = extensionAlias;
+
+    return config;
+  },
+};
 
 export default nextConfig;
