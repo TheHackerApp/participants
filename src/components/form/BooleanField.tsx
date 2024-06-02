672a9fc4082b24eea6d50a7a24dc0cde09cba9e0
Alex Krantz <alex@krantz.dev>
2024-06-01 21:08:32 -0700

    Add invalid state to boolean field

--- src/components/form/BooleanField.tsx
+++ src/components/form/BooleanField.tsx
@@ -5,25 +5,37 @@ import { useFormStatus } from 'react-dom';
 import { useController } from 'react-hook-form';
 import type { Control, ControllerRenderProps, FieldPathByValue, FieldValues } from 'react-hook-form';
 
-type Props<TFieldValues extends FieldValues, TPath extends FieldPathByValue<TFieldValues, boolean>> = Omit<
+type Props<TFieldValues extends FieldValues, TPath extends FieldPathByValue<TFieldValues, boolean | null>> = Omit<
   CheckboxProps,
   keyof ControllerRenderProps | 'value' | 'onValueChange' | 'required' | 'form'
 > & {
   control: Control<TFieldValues>;
   name: TPath;
+  default?: boolean;
 };
 
-const SwitchField = <TFieldValues extends FieldValues, TPath extends FieldPathByValue<TFieldValues, boolean>>({
+const SwitchField = <TFieldValues extends FieldValues, TPath extends FieldPathByValue<TFieldValues, boolean | null>>({
   control,
   name,
+  default: defaultValue,
   ...rest
 }: Props<TFieldValues, TPath>): ReactNode => {
   const {
     field: { onChange, value, ...field },
+    fieldState: { invalid },
   } = useController({ name, control });
   const { pending } = useFormStatus();
 
-  return <Checkbox isSelected={value} onValueChange={onChange} isDisabled={pending} {...rest} {...field} />;
+  return (
+    <Checkbox
+      isSelected={value ?? defaultValue}
+      onValueChange={onChange}
+      isDisabled={pending}
+      isInvalid={invalid}
+      {...rest}
+      {...field}
+    />
+  );
 };
 
 export default SwitchField;
