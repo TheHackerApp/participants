17506158a973183bc5e71f072af887f11b0823b2
Alex Krantz <alex@krantz.dev>
2024-06-02 15:19:29 -0700

    Mark required fields in review step

--- src/app/(dashboard)/@application/review/_components/Fields/Fields.tsx
+++ src/app/(dashboard)/@application/review/_components/Fields/Fields.tsx
@@ -15,22 +15,29 @@ const Fields = async (): Promise<ReactElement> => {
   return (
     <div className="space-y-4">
       <Group name="About You" href="/about">
-        <Row label="Given name" value={data.me.givenName} />
-        <Row label="Family name" value={data.me.familyName} />
-        <Row label="Gender" value={data.draftApplication?.gender} render={(value) => GENDER_NAMES[value]} />
+        <Row label="Given name" value={data.me.givenName} required />
+        <Row label="Family name" value={data.me.familyName} required />
+        <Row label="Gender" value={data.draftApplication?.gender} render={(value) => GENDER_NAMES[value]} required />
         <Row
           label="Race / Ethnicity"
           value={data.draftApplication?.raceEthnicity}
           render={(value) => RACE_ETHNICITY_NAMES[value]}
+          required
         />
-        <Row label="Birthday" value={data.draftApplication?.dateOfBirth} render={(value) => <Date value={value} />} />
+        <Row
+          label="Birthday"
+          value={data.draftApplication?.dateOfBirth}
+          render={(value) => <Date value={value} />}
+          required
+        />
       </Group>
       <Group name="Education" href="/education">
-        <Row label="Graduation year" value={data.draftApplication?.graduationYear} />
+        <Row label="Graduation year" value={data.draftApplication?.graduationYear} required />
         <Row
           label="Level of study"
           value={data.draftApplication?.education}
           render={(value) => EDUCATION_NAMES[value]}
+          required
         />
         <Row label="Major" value={data.draftApplication?.major} />
       </Group>
@@ -45,13 +52,13 @@ const Fields = async (): Promise<ReactElement> => {
         />
       </Group>
       <Group name="Shipping" href="/shipping">
-        <Row label="Address line 1" value={data.draftApplication?.addressLine1} />
+        <Row label="Address line 1" value={data.draftApplication?.addressLine1} required />
         <Row label="Address line 2" value={data.draftApplication?.addressLine2} />
         <Row label="Address line 3" value={data.draftApplication?.addressLine3} />
         <Row label="City" value={data.draftApplication?.locality} />
         <Row label="State / Province" value={data.draftApplication?.administrativeArea} />
-        <Row label="Postal code" value={data.draftApplication?.postalCode} />
-        <Row label="Country" value={data.draftApplication?.country} />
+        <Row label="Postal code" value={data.draftApplication?.postalCode} required />
+        <Row label="Country" value={data.draftApplication?.country} required />
       </Group>
     </div>
   );
--- src/app/(dashboard)/@application/review/_components/Fields/_components/Row.tsx
+++ src/app/(dashboard)/@application/review/_components/Fields/_components/Row.tsx
@@ -4,11 +4,14 @@ interface Props<T extends ReactNode> {
   label: string;
   value?: T | null;
   render?: (value: T) => ReactNode;
+  required?: boolean;
 }
 
-const Row = <T extends ReactNode>({ label, value, render = (value) => value }: Props<T>): ReactNode => (
+const Row = <T extends ReactNode>({ label, value, required, render = (value) => value }: Props<T>): ReactNode => (
   <div className="grid grid-cols-2">
-    <p className="font-medium">{label}:</p>
+    <p className="font-medium">
+      {label} {required && <span className="text-red-500">*</span>}
+    </p>
     <p>{value ? render(value) : ''}</p>
   </div>
 );
