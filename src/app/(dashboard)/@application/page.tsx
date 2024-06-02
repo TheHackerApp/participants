d7cc3d3a571ad0200f4dbb82bf01f8b902517dd6
Alex Krantz <alex@krantz.dev>
2024-06-02 15:16:43 -0700

    Determine correct application step for user

--- src/app/(dashboard)/@application/page.tsx
+++ src/app/(dashboard)/@application/page.tsx
@@ -1,9 +1,27 @@
 import { redirect } from 'next/navigation';
 import { ReactElement } from 'react';
 
+import { getClient } from '@/graphql/clients/server';
+
+import { RequiredApplicationFieldsDocument } from './RequiredApplicationFields.graphql';
+
 const Application = async (): Promise<ReactElement> => {
-  // TODO: determine current step from draft application
-  redirect('about');
+  const client = getClient();
+  const { data } = await client.query({ query: RequiredApplicationFieldsDocument });
+
+  if (!data.draftApplication) redirect(`about`);
+
+  if (!(data.draftApplication.gender && data.draftApplication.raceEthnicity && data.draftApplication.dateOfBirth))
+    redirect('about');
+
+  if (!(data.draftApplication.education && data.draftApplication.graduationYear)) redirect('education');
+
+  if (!data.draftApplication.hackathonsAttended) redirect('experience');
+
+  if (!(data.draftApplication.addressLine1 && data.draftApplication.postalCode && data.draftApplication.country))
+    redirect('shipping');
+
+  redirect('review');
 };
 
 export default Application;
